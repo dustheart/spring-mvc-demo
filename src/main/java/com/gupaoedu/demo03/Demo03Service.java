4eccc244e387cae29d9416c04daf8c71e952baef
juan.wang <juan.wang@itrigger.com>
2020-11-19 23:18:56 +0800

    spring framework注解驱动：3.x @ComponentScan

--- src/main/java/com/gupaoedu/demo03/Demo03Service.java
+++ src/main/java/com/gupaoedu/demo03/Demo03Service.java
@@ -1,4 +1,7 @@
 package com.gupaoedu.demo03;
 
+import org.springframework.stereotype.Service;
+
+@Service
 public class Demo03Service {
 }
--- src/main/java/com/gupaoedu/demo03/SpringConfiguration.java
+++ src/main/java/com/gupaoedu/demo03/SpringConfiguration.java
@@ -1,13 +1,14 @@
 package com.gupaoedu.demo03;
 
-import org.springframework.context.annotation.Bean;
+import org.springframework.context.annotation.ComponentScan;
 import org.springframework.context.annotation.Configuration;
 
+@ComponentScan("com.gupaoedu.demo03")
 @Configuration //相当于applicationContext.xml文件
 public class SpringConfiguration {
 
-	@Bean //相当于<bean />
-	public Demo03Service demo03Service(){
-		return new Demo03Service();
-	}
+//	@Bean //相当于<bean />
+//	public Demo03Service demo03Service(){
+//		return new Demo03Service();
+//	}
 }
